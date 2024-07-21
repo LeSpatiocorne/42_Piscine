e203ee5c25d86380f391395775539bb0f08cd72f
root <root@ordi.localdomain>
2024-07-21 19:06:20 +0200

    commit ex00 and ex01

--- c03/ex01/ft_strncmp.c
+++ c03/ex01/ft_strncmp.c
@@ -1,26 +1,28 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strcmp.c                                        :+:      :+:    :+:   */
+/*   ft_strncmp.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: root <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/21 13:48:01 by root              #+#    #+#             */
-/*   Updated: 2024/07/21 19:05:24 by root             ###   ########.fr       */
+/*   Updated: 2024/07/21 18:59:59 by root             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 //#include <stdio.h>
 
-int	ft_strcmp(char *s1, char *s2)
+int	ft_strncmp(char *s1, char *s2, unsigned int n)
 {
 	int	i;
 	int	diff;
 
 	i = 0;
 	diff = 0;
-	while (s1[i] || s2[i])
+	if (n == 0)
+		return (0);
+	while ((s1[i] || s2[i]) && i < n)
 	{
 		diff = diff + (s1[i] - s2[i]);
 		i++;
@@ -32,8 +34,8 @@ int	main()
 {
 	char	str1[] = "teste";
 	char	str2[] = "test";
-
-	printf("Comparaison entre %s et %s : %d\n", str1, str2, ft_strcmp(str1, str2));
+	unsigned int number = 5;
+	printf("Comparaison entre %s et %s : %d\n", str1, str2, ft_strncmp(str1, str2, number));
 	return (0);
 }
 */
