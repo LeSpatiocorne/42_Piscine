2bf363fe22396352409534be03e9757f65a55609
root <root@ordi.localdomain>
2024-07-21 14:49:13 +0200

    c03 ex00 finished

--- c03/ex00/ft_strcmp.c
+++ c03/ex00/ft_strcmp.c
@@ -6,8 +6,39 @@
 /*   By: root <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/21 13:48:01 by root              #+#    #+#             */
-/*   Updated: 2024/07/21 13:53:53 by root             ###   ########.fr       */
+/*   Updated: 2024/07/21 14:48:27 by root             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <unistd.c>
+#include <unistd.h>
+//#include <stdio.h>
+
+int	ft_strcmp(char *s1, char *s2)
+{
+	int	i;
+	int	i2;
+
+	i = 0;
+	i2 = 0;
+	while (s1[i] != '\0')
+		i++;
+	while (s2[i2] != '\0')
+		i2++;
+	if (i == i2)
+		return (0);
+	else if (i < i2)
+		return (-1);
+	else
+		return (1);
+}
+/*
+int	main(void)
+{
+	char *test1 = "hello";
+	char *test2 = "voidei";
+
+	printf("Test 1: \"%s\" vs \"%s\" => %d\n",
+		test1, test2, ft_strcmp(test1, test2));
+	return (0);
+}
+*/
