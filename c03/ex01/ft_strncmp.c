28dfc4346ac160cbeb2283500b613bd9f4e3933c
LeSpatiocorne <n2az.yt@gmail.com>
2024-07-23 23:15:40 +0200

    corrected index not being unsigned int in c03/ex01

--- c03/ex01/ft_strncmp.c
+++ c03/ex01/ft_strncmp.c
@@ -10,12 +10,11 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <unistd.h>
-//#include <stdio.h>
+#include <stdio.h>
 
 int	ft_strncmp(char *s1, char *s2, unsigned int n)
 {
-	int	i;
+	unsigned int	i;
 
 	i = 0;
 	if (n == 0)
