0ddc19ab3c8727b92b81313e38ee5bb1b3268e7d
Nicolas Druon <ndruon@c2r1p5.42perpignan.fr>
2024-07-23 11:46:06 +0200

    push changes

--- c02/ex09/ft_strcapitalize.c
+++ c02/ex09/ft_strcapitalize.c
@@ -6,7 +6,7 @@
 /*   By: ndruon <marvin@42.fr>                       +#+  +:+       +#+       */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/20 15:06:59 by ndruon             #+#    #+#            */
-/*   Updated: 2024/07/22 16:52:50 by ndruon           ###   ########.fr       */
+/*   Updated: 2024/07/23 11:42:36 by ndruon           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,12 +25,17 @@ char	*ft_strcapitalize(char	*str)
 		i++;
 	}
 	i = 0;
-	while (str[i] != '\0')
+	while (str[i])
 	{
 		if (i == 0)
 		       str[i] -= 32;
-		else if (!(str[i - 1] < 48 && str[i - 1] > 57) && !(str[i] < 21 && str[i] > 126)) 
-			str[i] -= 32;
+		else if ((str[i - 1] > 32 && str[i - 1] < 48) ||
+				(str[i - 1] > 90 && str[i - 1] < 97) ||
+				(str[i - 1] > 122 && str[i - 1] < 127))
+		{
+			if (!(str[i] > 96 && str[i] < 123))
+				str[i] -=32;
+		}
 		i++;
 	}
 	return (str);
