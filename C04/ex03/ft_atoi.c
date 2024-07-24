2d4d5cbb55bd94396f29f46cab4ab0ac029675c7
LeSpatiocorne <n2az.yt@gmail.com>
2024-07-24 19:00:36 +0200

    ft_atoi

--- C04/ex03/ft_atoi.c
+++ C04/ex03/ft_atoi.c
@@ -1,39 +1,43 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_putstr.c                                        :+:      :+:    :+:   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: root <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/07/24 14:18:46 by root              #+#    #+#             */
-/*   Updated: 2024/07/24 18:34:07 by root             ###   ########.fr       */
+/*   Created: 2024/07/24 17:24:55 by root              #+#    #+#             */
+/*   Updated: 2024/07/24 18:58:44 by root             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <unistd.h>
+#include <stdio.h>
 
-void	ft_putchar(char c)
-{
-	write(1, &c, 1);
-}
-
-void	ft_putstr(char *str)
+int	ft_atoi(char *str)
 {
 	int	i;
+	int	nb;
+	int	number;
 
 	i = 0;
-	while (str[i] != '\0')
+	number = 0;
+	nb = 1;
+	while (!(str[i] > 32 && str[i] < 127))
+		i++;
+	while (str[i] == '-' || str[i] == '+')
 	{
-		ft_putchar(str[i]);
+		if (str[i] == '-')
+			nb = -nb;
 		i++;
 	}
+	while (str[i] > 47 && str[i] < 58)
+		number = number * 10 + str[i++] - 48;
+	return (number * nb);
 }
 /*
 int	main(void)
 {
-	char str[] = "point virgule lol";
-	ft_putstr(str);
-	write(1, "\n", 1);
-	return (0);
+	char	str[] = " /+*";
+
+	printf("notre chiffre est : %d\n", ft_atoi(str));
 }
 */
