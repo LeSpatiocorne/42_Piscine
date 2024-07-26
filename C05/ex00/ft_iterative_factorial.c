b8c58c8d48aae1c678253ad71396f119e6063256
root <root@ordi>
2024-07-26 20:52:50 +0200

    corrected factorial

--- C05/ex00/ft_iterative_factorial.c
+++ C05/ex00/ft_iterative_factorial.c
@@ -6,7 +6,7 @@
 /*   By: ndruon <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/25 13:03:21 by ndruon            #+#    #+#             */
-/*   Updated: 2024/07/25 13:48:09 by ndruon           ###   ########.fr       */
+/*   Updated: 2024/07/26 20:52:12 by root             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,16 +20,13 @@ int ft_iterative_factorial(int nb)
 	if (nb < 0)
 		return (0);
 	while (nb > 0)
-	{
 		i *= nb--;
-		nb--;
-	}
 	return (i);
 }
 /*
 int	main(void)
 {
-	int nb = 4;
+	int nb = 5;
 	printf("nb = %d\nfactoriel = %d\n", nb, ft_iterative_factorial(nb));
 }
 */
