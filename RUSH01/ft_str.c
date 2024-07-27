74d564d32237f2bd87cbf474a7f300778753019d
Nicolas Druon <ndruon@c3r2p10.42perpignan.fr>
2024-07-27 16:44:36 +0200

    push little things done for rush

--- RUSH01/ft_str.c
+++ RUSH01/ft_str.c
@@ -1,30 +1,23 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   grid.c                                             :+:      :+:    :+:   */
+/*   ft_str.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ndruon <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/07/27 14:18:00 by ndruon            #+#    #+#             */
-/*   Updated: 2024/07/27 15:32:10 by ndruon           ###   ########.fr       */
+/*   Created: 2024/07/27 14:27:57 by ndruon            #+#    #+#             */
+/*   Updated: 2024/07/27 14:30:13 by ndruon           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-#include "header.h"
+
 #include <unistd.h>
 
-void	ft_grid(char *str)
+int	ft_strlen(char *str)
 {
-	int i;
-	char current;
+	int	i;
 
 	i = 0;
 	while (str[i] != '\0')
-	{
-		current = str[i];
-		write(1, &current, 1);
 		i++;
-		if (i == 4 || i == 8 || i == 12 || i == 16)
-			write(1, "\n", 1);
-	}
-	write(1, "\n", 1);
+	return (i);
 }
--- RUSH01/header.h
+++ RUSH01/header.h
@@ -1,7 +1,7 @@
-#ifndef
-#define
+#ifndef HEADER_H
+#define HEADER_H
 
-
-
+int	ft_strlen(char *str);
+void	ft_grid(char *str);
 
 #endif
