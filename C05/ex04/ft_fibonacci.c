5146830a3de7a42bdd7a22158e4107b7a21e78ae
Nicolas Druon <ndruon@c2r5p10.42perpignan.fr>
2024-07-30 14:04:53 +0200

    added fibonacci

--- C05/ex04/ft_fibonacci.c
+++ C05/ex04/ft_fibonacci.c
@@ -6,8 +6,15 @@
 /*   By: ndruon <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/25 13:06:36 by ndruon            #+#    #+#             */
-/*   Updated: 2024/07/25 13:06:39 by ndruon           ###   ########.fr       */
+/*   Updated: 2024/07/30 14:02:54 by ndruon           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-
+int	ft_fibonacci(int index)
+{
+	if (index < 0)
+		return (-1);
+	if (index <= 1)
+		return (index);
+	return (ft_fibonacci(index - 1) + ft_fibonacci(index - 2));
+}
