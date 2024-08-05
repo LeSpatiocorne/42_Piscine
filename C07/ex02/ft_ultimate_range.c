15d67e10276fe8266c536b802c360a8594a61530
LeSpatiocorne <97949654+LeSpatiocorne@users.noreply.github.com>
2024-08-05 14:33:12 +0200

    added ft_ultimate_range

---
+++ C07/ex02/ft_ultimate_range.c
@@ -0,0 +1,40 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+int     ft_ultimate_range(int **range, int min, int max)
+{
+    int     i;
+
+    i = 0;
+    min++;
+    if (min >= max)
+    {
+        *range = NULL;
+        return (0);
+    }
+    *range = malloc(sizeof(int) * (max - min));
+    while (min < max)
+        (*range)[i++] = min++;
+    return (i);
+}
+/*
+int main(void)
+{
+    int min = 5;
+    int max = 12;
+    int *result;
+    int size = ft_ultimate_range(&result, min, max);
+    
+    printf("min: %d\nmax: %d\nresult: ", min, max);
+    if (result)
+    {
+        for (int i = 0; i < size; i++)
+            printf("%d ", result[i]);
+        free(result);
+    }
+    else
+        printf("Memory allocation failed");
+    return 0;
+}
+*/
