67127d4cfa6cfd8fb9f80c1675d16ee7a58a0175
LeSpatiocorne <97949654+LeSpatiocorne@users.noreply.github.com>
2024-08-06 15:14:49 +0200

    updated square to my own method

--- C05/ex05/ft_sqrt.c
+++ C05/ex05/ft_sqrt.c
@@ -10,35 +10,29 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
-#include <stdlib.h>
-
-int	ft_sqrt_recursive(int nb, int start, int end)
-{
-	int	square;
-	int	mid;
-
-	if (start > end)
-		return (-1);
-	mid = (start + end) / 2;
-	square = mid * mid;
-	if (square == nb)
-		return (mid);
-	else if (square < nb)
-		return (ft_sqrt_recursive(nb, mid + 1, end));
-	else
-		return (ft_sqrt_recursive(nb, start, mid - 1));
-}
+//#include <stdio.h>
+//#include <stdlib.h>
 
 int	ft_sqrt(int nb)
 {
-	return (ft_sqrt_recursive(nb, 0, nb));
+	int	i;
+
+	i = 0;
+	while (i * i <= nb)
+	{
+		if (i * i == nb)
+			return (i);
+		i++;
+	}
+	return (-1);
 }
+
 /*
 int main(void)
 {
-	int number = 8;
+	int number = 4120900;
 	int result = ft_sqrt(number);
 	printf("The square root of %d is %d\n", number, result);
 	return 0;
-}*/
+}
+*/
