908892781d0d668af3a863d89fe704f6ae405885
Nicolas Druon <nidruon@c2r3p10.42perpignan.fr>
2024-09-05 16:43:20 +0200

    comb2

--- C00/ex06/ft_print_comb2.c
+++ C00/ex06/ft_print_comb2.c
@@ -6,16 +6,52 @@
 /*   By: nidruon <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/05 14:20:27 by nidruon           #+#    #+#             */
-/*   Updated: 2024/09/05 14:46:29 by nidruon          ###   ########.fr       */
+/*   Updated: 2024/09/05 16:40:08 by nidruon          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include <unistd.h>
+#include <stdbool.h>
 
-void	ft_ftputchar(char c)
+void	ft_putchar(char c)
 {
 	write(1, &c, 1);
 }
 
+void	ft_aff_comb(int a, int b, bool t)
+{
+	ft_putchar((a + 48) % 10);
+	ft_putchar((a + 48) / 10);
+	ft_putchar(' ');
+	ft_putchar((b + 48) % 10);
+	ft_putchar((b + 48) / 10);
+	if (t)
+	{
+		ft_putchar(',');
+		ft_putchar(' ');
+	}
+}
 void	ft_print_comb2(void)
 {
-	int	
+	int	a;
+	int	b;
+	bool	t;
+
+	a = 0;
+	while (a <= 99)
+	{
+		b = a + 1;
+		while (b <= 99)
+		{
+			t = !(a == 98 && b == 99);
+			ft_aff_comb(a, b, t);
+			b++;
+		}
+		a++;
+	}
+}
+
+int main(void)
+{
+	ft_print_comb2();
+	return (0);
+}
