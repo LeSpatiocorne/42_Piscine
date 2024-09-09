c822f08232aadec658b50a7a9efc2a6d10f1aa6d
LeSpatiocorne <97949654+LeSpatiocorne@users.noreply.github.com>
2024-09-09 13:51:37 +0200

    renormed code

--- C02/ex09/ft_strcapitalize.c
+++ C02/ex09/ft_strcapitalize.c
@@ -28,24 +28,24 @@ char	*ft_strcapitalize(char	*str)
 	while (str[i])
 	{
 		if (i == 0)
-		       str[i] -= 32;
-		else if ((str[i - 1] > 32 && str[i - 1] < 48) ||
-				(str[i - 1] > 90 && str[i - 1] < 97) ||
-				(str[i - 1] > 122 && str[i - 1] < 127))
+			str[i] -= 32;
+		else if ((str[i - 1] > 32 && str[i - 1] < 48)
+			|| (str[i - 1] > 90 && str[i - 1] < 97)
+			|| (str[i - 1] > 122 && str[i - 1] < 127))
 		{
 			if (!(str[i] > 96 && str[i] < 123))
-				str[i] -=32;
+				str[i] -= 32;
 		}
 		i++;
 	}
 	return (str);
 }
-
+/*
 int	main(void)
 {
-	char	str[] = "sAlUt, comment tu vas ? 42mots quarante-deux; cinquante+et+un";
+	char	str[] = "sAlUt, comment tu vas ? 
+	42mots quarante-deux; cinquante+et+un";
 	printf("%s", ft_strcapitalize(str));
 	return 0;
 }
-
-
+*/
