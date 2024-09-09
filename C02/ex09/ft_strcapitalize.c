dafac95fdb400844f88270aeebb82034ebc52e15
LeSpatiocorne <97949654+LeSpatiocorne@users.noreply.github.com>
2024-09-09 21:05:11 +0200

    updated capitalize

--- C02/ex09/ft_strcapitalize.c
+++ C02/ex09/ft_strcapitalize.c
@@ -25,27 +25,21 @@ char	*ft_strcapitalize(char	*str)
 		i++;
 	}
 	i = 0;
+	if ((str[0] >= 'a') && (str[0] <= 'z'))
+		str[0] -= 32;
 	while (str[i])
 	{
-		if (i == 0)
+		if ((str[i - 1] < 47) && ((str[i] >= 'a') && (str[i] <= 'z')))
 			str[i] -= 32;
-		else if ((str[i - 1] > 32 && str[i - 1] < 48)
-			|| (str[i - 1] > 90 && str[i - 1] < 97)
-			|| (str[i - 1] > 122 && str[i - 1] < 127))
-		{
-			if (!(str[i] > 96 && str[i] < 123))
-				str[i] -= 32;
-		}
 		i++;
 	}
 	return (str);
 }
+
 /*
 int	main(void)
 {
-	char	str[] = "sAlUt, comment tu vas ? 
-	42mots quarante-deux; cinquante+et+un";
-	printf("%s", ft_strcapitalize(str));
+	printf("%s", ft_strcapitalize("sAlUt, comment tu vas ? 
+	42mots quarante-deux; cinquante+et+un"));
 	return 0;
-}
-*/
+}*/
