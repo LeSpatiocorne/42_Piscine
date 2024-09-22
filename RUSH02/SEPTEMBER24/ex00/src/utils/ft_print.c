24bf6bc733b51ba5fb5741a2b88595f3f19dbe47
LeSpatiocorne <97949654+LeSpatiocorne@users.noreply.github.com>
2024-09-22 17:20:48 +0200

    RUSH02 with ft-atoa lol

--- RUSH02/SEPTEMBER24/ex00/src/utils/ft_print.c
+++ RUSH02/SEPTEMBER24/ex00/src/utils/ft_print.c
@@ -24,52 +24,53 @@ void	ft_print_tens(char *str, t_list *dict, int i)
 	}
 }
 
-void	ft_extract_numbers(char *str, char **dest)
+char	*ft_atoi(char *str)
 {
 	int		i;
-	int		j;
-	int		size;
+	int		start;
+	char	*result;
 
 	i = 0;
-	while (str[i] && (str[i] < '0' || str[i] > '9'))
+	while (str[i] == ' ' || (str[i] >= 9 && str[i] <= 13))
 		i++;
-	j = i - 1;
-	size = 0;
-	while (j++, str[j] >= '0' && str[j] <= '9')
-		size++;
-	if (size > 0 && size <= 39)
-	{
-		*dest = malloc((size + 1) * sizeof(char));
-		j = 0;
-		while (str[i] >= '0' && str[i] <= '9')
-			(*dest)[j++] = str[i++];
-		(*dest)[j] = '\0';
-	}
-	else
-	{
-		*dest = NULL;
+	if (str[i] == '+')
+		i++;
+	else if (str[i] == '-' || str[i] == '+')
 		ft_error(3);
-	}
+	start = i;
+	while (str[i] >= '0' && str[i] <= '9')
+		i++;
+	if (i == start)
+		ft_error(3);
+	result = (char *)malloc(sizeof(char) * (i - start + 1));
+	if (!result)
+		return (NULL);
+	i = -1;
+	while (i++, str[start + i] && str[start + i] >= '0' && str[start + i] <= '9')
+		result[i] = str[start + i];
+	result[i] = '\0';
+	return (result);
 }
+
 
 void	ft_print(char *str, char *file)
 {
 	char	*dest;
 
-	while (*str && (*str < '0' || *str > '9'))
-	{
-		if (*str == '-' && (*(str + 1) >= '0' && *(str + 1) <= '9'))
-			ft_error(3);
-		ft_putchar(*str++);
-	}
-	ft_extract_numbers(str, &dest);
+	dest = ft_atoi(str);
 	if (dest)
 	{
-		ft_str_numbers(dest, file);
-		free(dest);
-		while (*str >= '0' && *str <= '9')
-			str++;
+		if (ft_strlen(dest) > 0 && ft_strlen(dest) <= 39)
+		{
+			ft_str_numbers(dest, file);
+			free(dest);
+		}
+		else
+		{
+			free(dest);
+			ft_error(3);
+		}
 	}
-	if (*str)
-		ft_print(str, file);
+	else
+		ft_error(3);
 }
